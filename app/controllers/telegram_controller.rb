97c63295943d74b8f6aacb95beb9ac1316373e4c
alexander-leitch <alexander.leitch@gmail.com>
2017-06-29 22:13:57 +0200

    removing token from src

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -3,7 +3,7 @@ class TelegramsController < ApplicationController
   # skip_before_action :verify_authenticity_token, only: [:create, :show]
   
   def create
-    token = "110515929:AAG58jgMlIEZ3dvT9erZ-Mgf3__Bq7E8OkY"
+    token = ENV["TELEGRAM_TOKEN"]
     
     update = Telegrammer::DataTypes::Update.new(
       update_id: params[:update_id],
